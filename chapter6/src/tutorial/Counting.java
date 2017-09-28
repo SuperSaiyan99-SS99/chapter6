b7398fb72b62955556db598364242212577fbcc0
SuperSaiyan99-SS99 <SuperSaiyan99-SS99@users.noreply.github.com>
2017-09-28 09:26:37 -0500

    save

---
+++ chapter6/src/tutorial/Counting.java
@@ -0,0 +1,24 @@
+package tutorial;
+
+public class Counting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int y = 0;
+		int z = 0;
+		
+		for (int x = 0; x <= 10; x ++ ) {
+			System.out.println(x);
+		}
+		while (y <= 10) {
+			System.out.println(y);
+			y ++;
+		}
+		do {
+			System.out.println(z);
+			z ++;
+		}
+		while (z <= 10);
+	}
+
+}
