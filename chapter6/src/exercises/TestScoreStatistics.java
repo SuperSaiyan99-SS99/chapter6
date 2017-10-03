893cb5986feb42e348a56f0cdd1316d982f82441
SuperSaiyan99-SS99 <SuperSaiyan99-SS99@users.noreply.github.com>
2017-10-02 22:09:24 -0500

    Add highest and lowest values

--- chapter6/src/exercises/TestScoreStatistics.java
+++ chapter6/src/exercises/TestScoreStatistics.java
@@ -6,10 +6,12 @@ public class TestScoreStatistics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int score = 0;
+		int [] scores = new int [];
+		int score;
+		int count = 0;
 		int total = 0;
-		int highest;
-		int lowest;
+		int highest = 0;
+		int lowest = 0;
 		final int MIN = 0;
 		final int MAX = 100;
 		final int QUIT = 999;
@@ -18,22 +20,38 @@ public class TestScoreStatistics {
 		System.out.println("Enter quiz score or " + QUIT + " to quit >> ");
 		score = input.nextInt();
 		
-		while(score != quit) {
-			scores[count] = score;
-			total += scores[count];
-			++count;
-			if(count == max)
-				score = quit;
-			else
+		while(score != QUIT) {
+			if (MIN <= score <= MAX) {
+				scores[count] = score;
+				total += scores[count];
+				if (scores[count] > highest) {
+					highest = scores[count];
+				}
+				if (scores[count] < lowest) {
+					lowest = scores[count];
+				}
+				++count;
 				System.out.println("Enter next quiz score or " + QUIT + " to quit >> ");
 				score = input.nextInt();
+			}
+			else {
+				System.out.println("That score is no good.");
+				System.out.println("Enter next quiz score or " + QUIT + " to quit >> ");
+				score = input.nextInt();
+			}
 		}
 		System.out.print("The scores enterd were: ");
-		for(int x = 0; x < count; ++ x)
+		for(int x = 0; x < count; ++ x) {
 			System.out.println(scores[x] + " ");
-		if(count != 0)
+		}
+		if(count != 0) {
 			System.out.println("\n The average is " + (total * 1) / count);
-		else System.out.println("No scores entered");
+			System.out.println("The highest is " + highest);
+			System.out.println("The lowest is " + lowest);
+		}
+		else {
+			System.out.println("No scores entered");
+		}
 	}
 
 }
